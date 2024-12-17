33135feb87b1c63ddbb5d7f977348ff9577a5d47
Mukhammed <muxalllsss@gmail.com>
2024-12-17 17:11:09 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar/Navbar";
+import PixelCraft from './Components/PixelCraft/PixelCraft';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+      <PixelCraft />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+import { FaTelegram } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <div>
+          <header>
+            <nav>
+            <div className='a'>
+            <img src='logo.png' />
+            <ul>
+            <a className='about' href='#about'>О нас</a>
+            <a className='projects' href="#projects">Проекты</a>
+            <a className='otzyvy' href="#otzyvy">Отзывы</a>
+            <a className='partners' href='#partners'>Партнеры</a>
+            </ul>
+            <a href='https://t.me/pixelcraft_me' target='blank'><button>Обсудить проект</button></a>
+            </div>
+            </nav>
+        </header>
+    </div>
+  );
+};
+
+export default Navbar;
